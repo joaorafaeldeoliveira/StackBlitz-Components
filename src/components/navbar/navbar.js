b9876548fbf85c848947a91dc5d506176fcc7746
joaorafaeldeoliveira <jrojoao@gmail.com>
2025-10-29 16:48:57 -0300

    Navbars

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById('menuButton');
+const navLinks = document.getElementById('navLinks');
+
+menuButton.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
