75b800fc5802c4a5db6defec5b16eda748bfd9c0
joaorafaeldeoliveira <jrojoao@gmail.com>
2025-10-29 15:41:02 -0300

    Change modal.js

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,9 +1,11 @@
+
 const modal = document.getElementById('modal');
 document.getElementById('openModal').onclick = () => modal.classList.add('show');
 document.getElementById('closeModal').onclick = () => modal.classList.remove('show');
 modal.addEventListener('click', e => {
   if (e.target === modal) modal.classList.remove('show');
 });
+
 
 const confirmModal = document.getElementById('confirmModal');
 document.getElementById('openConfirm').onclick = () => confirmModal.classList.add('show');
@@ -12,20 +14,24 @@ document.getElementById('confirmDelete').onclick = () => {
   alert("Item excluído!");
   confirmModal.classList.remove('show');
 };
-modal.addEventListener('click', e => {
-  if (e.target === confirmModal)confirmModal.classList.remove('show');
+confirmModal.addEventListener('click', e => {
+  if (e.target === confirmModal) confirmModal.classList.remove('show');
 });
 
 
 const formModal = document.getElementById('formModal');
-  const userForm = document.getElementById('userForm');
-  
-  document.getElementById('openForm').onclick = () => formModal.classList.add('show');
-  document.getElementById('cancelForm').onclick = () => formModal.classList.remove('show');
+const userForm = document.getElementById('userForm');
 
-  userForm.addEventListener('submit', e => {
-    e.preventDefault();
-    alert("Usuário cadastrado com sucesso!");
-    userForm.reset();
-    formModal.classList.remove('show');
-  });
+document.getElementById('openForm').onclick = () => formModal.classList.add('show');
+document.getElementById('cancelForm').onclick = () => formModal.classList.remove('show');
+
+userForm.addEventListener('submit', e => {
+  e.preventDefault();
+  alert("Usuário cadastrado com sucesso!");
+  userForm.reset();
+  formModal.classList.remove('show');
+});
+
+formModal.addEventListener('click', e => {
+  if (e.target === formModal) formModal.classList.remove('show');
+});
