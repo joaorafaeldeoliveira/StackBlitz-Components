5caa5f316355aa13cba1f276bc22c8b491bad38a
joaorafaeldeoliveira <jrojoao@gmail.com>
2025-10-29 12:09:17 -0300

    More modals

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -12,3 +12,20 @@ document.getElementById('confirmDelete').onclick = () => {
   alert("Item excluído!");
   confirmModal.classList.remove('show');
 };
+modal.addEventListener('click', e => {
+  if (e.target === confirmModal)confirmModal.classList.remove('show');
+});
+
+
+const formModal = document.getElementById('formModal');
+  const userForm = document.getElementById('userForm');
+  
+  document.getElementById('openForm').onclick = () => formModal.classList.add('show');
+  document.getElementById('cancelForm').onclick = () => formModal.classList.remove('show');
+
+  userForm.addEventListener('submit', e => {
+    e.preventDefault();
+    alert("Usuário cadastrado com sucesso!");
+    userForm.reset();
+    formModal.classList.remove('show');
+  });
