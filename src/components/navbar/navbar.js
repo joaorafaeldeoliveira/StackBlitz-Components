7b3ae87aefd51c91b1df75cfd860400e9b0ffd71
null <null>
2025-10-30 10:38:16 -0300

    SearchNavbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,3 +4,16 @@ const navLinks = document.getElementById('navLinks');
 menuButton.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.getElementById('searchButton');
+  const searchInput = document.getElementById('searchInput');
+
+  searchButton.addEventListener('click', () => {
+    if(searchInput.value === '') { 
+      alert('EITAAA')
+    }
+    alert(`Você pesquisou: ${searchInput.value}`);
+    console.log('VAAAAAAAAAAAAAAAAAA')
+  });
+});
